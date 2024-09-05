14b08ba33e1ab7a114b99183d3061fb2036fb69d
2°F <beatriz.l.santos7@aluno.senai.br>
2024-09-05 11:36:30 -0300

    uwqgyua

---
+++ estrutura_repeticao/exercicios/exe03.js
@@ -0,0 +1,9 @@
+let numero, validacao
+
+do {
+    numero = parseInt(prompt("Digite um número"))
+    if(isNaN(numero) || numero < 0){ 
+        alert("Número invalido, digite um valor positivo e maior que 0")
+
+    }
+} while (isNaN(numero2) || numero2 < 0);
