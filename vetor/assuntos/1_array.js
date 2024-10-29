603b32d430816ad2abbcaf03bffaae7cca77aa6b
2°F <beatriz.l.santos7@aluno.senai.br>
2024-10-29 07:30:25 -0300

    vbuv

---
+++ vetor/assuntos/1_array.js
@@ -0,0 +1,17 @@
+let var1 = "String"
+let var2 = 3 //number
+let var3 = false //booleanos
+
+//criando um Array (Vetor)
+let diasDaSemana = ["Domingo", "Segundo", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+console.log(diasDaSemana)
+
+//Atribuindo valores em Array
+let numeros = []
+numeros[0] = 42 
+numeros[1] = 30
+numeros[2] = 21
+numeros[4] = 89 
+console.log(numeros)
+//Mostrar elementos de um array
+console.log(numeros[1] + numeros[4])
